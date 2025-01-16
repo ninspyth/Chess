29de2e983bf2998c3661242b783135b480331185
aman <amandavu11@gmail.com>
2025-01-16 17:44:45 +0530

    Added OOP and check logic(not working)

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <raylib.h>
 #include <bits/stdc++.h>
 
-using namespace std;
+// ---------- TODO -------------
+// 1. Utilize OOP for the pieces ( Done )
+// 2. Add Castling
+// 3. Add en-passant
+// 4. Add logic for check and checkmate
 
-typedef struct Vec2 {
-    int x;
-    int y;
-} Vec2;
+
+// ---------- Issues -----------
+// 1. Check logic and Checkmate logic are not working
+
+using namespace std;
 
 const int SCREEN_WIDTH = 1024 + 128*3;
 const int SCREEN_HEIGHT = 1024;
 const int ROWS = 8;
 const int COLS = 8;
+bool no_moves_for_king = false;
+bool Check = false;
+
+typedef struct Vec2 {
+    int x;
+    int y;
+} Vec2;
 
 bool in_range(int num, int num1, int low, int high) {
     if(num <= high && num >= low && num1 >= low && num1 <= high) {
@@ -27,7 +39,113 @@ bool same_case(char a, char b) {
      return false;
 }
 
-void show_diagonal_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level) {
+bool is_opponent_king_check(vector<vector<char>> &board, vector<pair<int, int>> &possible_squares, char piece) {
+    for(pair<int, int> p: possible_squares) {
+        //for light pieces
+        if(isupper(piece)) {
+            if(board[p.second][p.first] == 'k') {
+                return true;
+            }
+        }
+
+        //for dark pieces
+        else {
+            if(board[p.second][p.first] == 'K') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+class Piece {
+    public:
+        //piece moves
+        vector<pair<int, int>> piece_moves;
+
+        // to show diagonal possible moves
+        int show_diagonal_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level);
+
+        // to show horizontal possible moves
+        int show_moves_along_axes(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level);
+
+        //to show rest possible moves
+        virtual int show_rest_possible_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece)= 0; 
+
+        //destructor
+        virtual ~Piece() = default;
+};
+
+class Knight: public Piece {
+    public:
+        int show_rest_possible_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece) override {
+           //eligible moves for knight
+            piece_moves = {{y-1, x+2}, {y+1, x-2}, {y-2, x+1}, {y+2, x+1}, {y-2, x-1}, {y-1, x-2}, {y+2, x-1}, {y+1, x+2}};
+            
+            //White Knight
+            if(piece == 'N') {
+                for(const pair<int, int> &moves: piece_moves) {
+                    //check for out of bounds and if the to pos is empty or enemy piece then move
+                    if(moves.first >=0 && moves.first <=7 && moves.second >=0 && moves.second <= 7 && (board[moves.second][moves.first] == ' ' || islower(board[moves.second][moves.first]))) {
+                        printf("first = %d, second = %d\n", moves.first, moves.second);
+                        possible_squares.push_back(moves);
+                    }
+                }
+            }
+
+            //Black Knight
+            else {
+                for(const pair<int, int> &moves: piece_moves) {
+                    //check for out of bounds and if the to pos is empty or enemy piece then move
+                    if(in_range(moves.first, moves.second, 0, 7) && (board[moves.second][moves.first] == ' ' || isupper(board[moves.second][moves.first]))) {
+                        possible_squares.push_back(moves);
+                    }
+                }
+            }
+
+            return possible_squares.size();
+        }
+};
+
+class Pawn: public Piece {
+    public:
+        int show_rest_possible_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece) override {
+            if(piece == 'P'){
+                if(in_range(x-1, y, 0, 7) && board[x-1][y] == ' ') {
+                    possible_squares.push_back({y,x-1});
+                }
+                if(x == 6 && board[x-2][y] == ' ') {
+                    possible_squares.push_back({y,x-2});
+                }
+                if(in_range(x-1, y+1, 0, 7) && islower(board[x-1][y+1])) {
+                    possible_squares.push_back({y+1, x-1});
+                }
+                if(in_range(x-1, y-1, 0, 7) && islower(board[x-1][y-1])) {
+                    possible_squares.push_back({y-1, x-1});
+                }
+            }
+
+            //eligible moves for black pawn
+            else if(piece == 'p') {
+                if(in_range(x+1, y, 0, 7) && board[x+1][y] == ' ') {
+                    possible_squares.push_back({y, x+1});
+                }
+                if(x== 1 && board[x+2][y] == ' ') {
+                    possible_squares.push_back({y, x+2});
+                }
+                if(in_range(x+1, y-1, 0, 7) && isupper(board[x+1][y-1])) {
+                    possible_squares.push_back({y-1, x+1});
+                }
+                if(in_range(x+1, y+1, 0, 7) && isupper(board[x+1][y+1])) {
+                    possible_squares.push_back({y+1, x+1});
+                }
+            }
+
+            return possible_squares.size();
+        }
+};
+
+int Piece::show_diagonal_moves(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level) {
     int level = 1, piece_x = x, piece_y = y;
     bool q1 = true, q2 = true, q3 = true, q4 = true;
     while(level <= max_level) {
@@ -85,9 +203,10 @@ void show_diagonal_moves(vector<vector<char>> &board, int x, int y, vector<pair<
         }
         level++;
     }
+    return possible_squares.size();
 }
 
-void show_moves_along_axes(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level) {
+int Piece::show_moves_along_axes(vector<vector<char>> &board, int x, int y, vector<pair<int, int>> &possible_squares, char piece, int max_level) {
     int level = 1;
     bool q1 = true, q2 = true, q3 = true, q4 = true;
     while(level <= max_level) {
@@ -141,83 +260,57 @@ void show_moves_along_axes(vector<vector<char>> &board, int x, int y, vector<pai
         }
         level++;
     }   
+    return possible_squares.size();
 }
 
-vector<pair<int, int>> showPossibleSquares(vector<vector<char>> &board, int x, int y, char piece){
-    vector<pair<int, int>> possible_squares, piece_moves;
 
-    if(piece == 'P'){
-        if(in_range(x-1, y, 0, 7) && board[x-1][y] == ' ') {
-            possible_squares.push_back({y,x-1});
-        }
-        if(x == 6 && board[x-2][y] == ' ') {
-            possible_squares.push_back({y,x-2});
-        }
-        if(in_range(x-1, y+1, 0, 7) && islower(board[x-1][y+1])) {
-            possible_squares.push_back({y+1, x-1});
-        }
-        if(in_range(x-1, y-1, 0, 7) && islower(board[x-1][y-1])) {
-            possible_squares.push_back({y-1, x-1});
-        }
-    }
-    else if(piece == 'p') {
-        if(in_range(x+1, y, 0, 7) && board[x+1][y] == ' ') {
-            possible_squares.push_back({y, x+1});
-        }
-        if(x== 1 && board[x+2][y] == ' ') {
-            possible_squares.push_back({y, x+2});
-        }
-        if(in_range(x+1, y-1, 0, 7) && isupper(board[x+1][y-1])) {
-            possible_squares.push_back({y-1, x+1});
-        }
-        if(in_range(x+1, y+1, 0, 7) && isupper(board[x+1][y+1])) {
-            possible_squares.push_back({y+1, x+1});
-        }
+vector<pair<int, int>> showPossibleSquares(vector<vector<char>> &board, int x, int y, char piece) {
+
+    Knight k;
+    Pawn p;
+
+    vector<pair<int, int>> possible_squares;
+
+    int no_of_moves = 0;
+
+    //pawn moves
+    if(piece == 'P' || piece == 'p') {
+        no_of_moves = p.show_rest_possible_moves(board, x, y, possible_squares, piece);
     }
 
-    //Knight possible moves
+    //Knight moves
     else if(piece == 'N' || piece == 'n') {
-        piece_moves = {{y-1, x+2}, {y+1, x-2}, {y-2, x+1}, {y+2, x+1}, {y-2, x-1}, {y-1, x-2}, {y+2, x-1}, {y+1, x+2}};
-        
-        if(piece == 'N') {
-            for(const pair<int, int> &moves: piece_moves) {
-                //check for out of bounds and if the to pos is empty or enemy piece then move
-                if(moves.first >=0 && moves.first <=7 && moves.second >=0 && moves.second <= 7 && (board[moves.second][moves.first] == ' ' || islower(board[moves.second][moves.first]))) {
-                    printf("first = %d, second = %d\n", moves.first, moves.second);
-                    possible_squares.push_back(moves);
-                }
-            }
+        no_of_moves = k.show_rest_possible_moves(board, x, y, possible_squares, piece);
+    }
+
+    //bishop moves
+    else if(piece == 'B' || piece == 'b') {
+        no_of_moves = k.show_diagonal_moves(board, x, y, possible_squares, piece, 7);
+    }
+
+    //king moves
+    else if(piece == 'K' || piece == 'k') {
+        no_of_moves += k.show_moves_along_axes(board, x, y, possible_squares, piece, 1);
+        no_of_moves += k.show_diagonal_moves(board, x, y, possible_squares, piece, 1);
+
+        //Checking for checkmate
+        if(no_of_moves == 0) {
+            no_moves_for_king = true;
         }
         else {
-            for(const pair<int, int> &moves: piece_moves) {
-                //check for out of bounds and if the to pos is empty or enemy piece then move
-                if(in_range(moves.first, moves.second, 0, 7) && (board[moves.second][moves.first] == ' ' || isupper(board[moves.second][moves.first]))) {
-                    possible_squares.push_back(moves);
-                }
-            }
+            no_moves_for_king = false;
         }
-    } 
-
-    //bishop possible moves
-    else if(piece == 'B' || piece == 'b') {
-        show_diagonal_moves(board, x, y, possible_squares, piece, 7);
     }
 
-    //king
-    else if(piece == 'K' || piece == 'k') {
-        show_moves_along_axes(board, x, y, possible_squares, piece, 1);
-        show_diagonal_moves(board, x, y, possible_squares, piece, 1);
-    }
-
-    //queen
+    //queen moves
     else if(piece == 'Q' || piece == 'q') {
-        show_diagonal_moves(board, x, y, possible_squares, piece, 7);
-        show_moves_along_axes(board, x, y, possible_squares, piece, 7);
+        no_of_moves += k.show_diagonal_moves(board, x, y, possible_squares, piece, 7);
+        no_of_moves += k.show_moves_along_axes(board, x, y, possible_squares, piece, 7);
     }
 
-    //rook
+    //rook moves
     else if(piece == 'R' || piece == 'r') {
-        show_moves_along_axes(board, x, y, possible_squares, piece, 7);
+        no_of_moves = k.show_moves_along_axes(board, x, y, possible_squares, piece, 7);
     }
 
     return possible_squares;
@@ -235,13 +328,14 @@ bool move_piece_from_to(vector<vector<char>> &board, vector<pair<int, int>> move
         if(moves.first == to.x && moves.second == to.y) {
             board[to.y][to.x] = board[from.y][from.x];
             board[from.y][from.x] = ' ';
+
+            
             return true;
         }
     }
     return false;
 }
 
-bool Checkmate = false;
 
 int main() {
     //chess board
@@ -347,6 +441,7 @@ int main() {
     // ToggleFullscreen();
 
     vector<pair<int, int>> move_piece_pos(2, {-1, -1});
+    vector<pair<int, int>> possible_moves;
 
     RenderTexture2D boardTexture = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -425,14 +520,11 @@ int main() {
         // // Drawing light canvas on right
         // DrawTexture(light_canvas, 128*8, 128*4, WHITE);
 
-        if(Checkmate) {
-           //End Game 
-        }
 
         if(selected_piece_x != -1){                       // this means a piece is selected
           vector<pair<int, int>> possible_squares = showPossibleSquares(board, selected_piece_y, selected_piece_x, board[selected_piece_y][selected_piece_x]);
           for(auto it: possible_squares){
-            DrawCircle(it.first*128 + (128/2), it.second*128 + (128/2), 15, GREEN);
+            DrawCircle(it.first*128 + (128/2), it.second*128 + (128/2), 15, BLACK);
           }
         }
 
@@ -442,17 +534,20 @@ int main() {
 
             cout << mouse_x << " " << mouse_y << endl;
 
+            //Check if the selected square is in bounds
+            //moves for light pieces
             if(light_turn && mouse_x <=7 && mouse_y <= 7){
+
+                //We want to move the selected piece
                 if(isupper(board[mouse_y][mouse_x])){
                     if(selected_piece_x == -1 && selected_piece_y == -1 || (mouse_x != selected_piece_x || mouse_y != selected_piece_y)) {
-                        cout << "hello" << "\n";
                         move_piece_pos[0] = {mouse_x, mouse_y};
                     }
                     selected_piece_x = mouse_x;               //findng the x, y of selected piece
                     selected_piece_y = mouse_y;
-                    // printf("from.x = %d, from.y = %d\n", move_piece_pos[0].first, move_piece_pos[0].second);
-                    // printf("piece_x = %d, piece_y = %d\n", selected_piece_x, selected_piece_y);
                 }
+
+                //We want to move the selected piece to the selected square  
                 else{
                     if(selected_piece_x == -1 && islower(board[mouse_y][mouse_x])) {
                         cout << "Invalid piece" << endl;
@@ -465,6 +560,15 @@ int main() {
                         move_piece_pos[1] = {mouse_x, mouse_y};
                         isMoved = move_piece_from_to(board, move_piece_pos);
                         if(isMoved) {
+                            
+                            //Checking if the opponent king is in check
+                            possible_moves = showPossibleSquares(board, mouse_y, mouse_x, board[mouse_y][mouse_x]);
+                            Check = is_opponent_king_check(board, possible_moves, board[mouse_y][mouse_x]);
+
+                            if(Check && no_moves_for_king) {
+                                cout << "White Won!!" << endl;
+                            }
+
                             move_piece_pos = {{-1, -1}, {-1, -1}};
                             light_turn = false;
                             dark_turn = true;
@@ -474,24 +578,38 @@ int main() {
                     selected_piece_y = -1;
                 }  
             }
+            
+            //check if selected square is in bounds
+            //moves for dark piece
             else if(dark_turn && mouse_x <=7 && mouse_y <= 7) {
+                
+                //We want to move the selected piece 
                 if(islower(board[mouse_y][mouse_x])) {
                     if(selected_piece_x == -1 && selected_piece_y == -1 || (mouse_x != selected_piece_x || mouse_y != selected_piece_y)) {
-                        cout << "hello" << "\n";
                         move_piece_pos[0] = {mouse_x, mouse_y};
                     }
                     selected_piece_x = mouse_x;
                     selected_piece_y = mouse_y;
                 }
+
+                //We want to move the selected piece to the selected square
                 else{
                     if(selected_piece_x == -1 && isupper(board[mouse_y][mouse_x])) {
                         cout << "Invalid Piece" << endl;
                     }
                     else {
-                        // cout << "move logic" << "\n";
                         move_piece_pos[1] = {mouse_x, mouse_y};
                         isMoved = move_piece_from_to(board, move_piece_pos);
                         if(isMoved) {
+
+                            //Checking if the opponent king is in check
+                            possible_moves = showPossibleSquares(board, mouse_x, mouse_y, board[mouse_y][mouse_x]);
+                            Check = is_opponent_king_check(board, possible_moves, board[mouse_y][mouse_x]);
+
+                            if(Check && no_moves_for_king) {
+                                cout << "White Won!!" << endl;
+                            }
+
                             move_piece_pos = {{-1, -1}, {-1, -1}};
                             light_turn = true;
                             dark_turn = false;
